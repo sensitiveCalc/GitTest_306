bdf24df24fd2b79ce2da569d901319852b5909bb
jeje <pearl903425@gmail.com>
2026-02-09 16:04:43 +0900

    시나리오 3번 - GoodsService 수정

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -4,10 +4,9 @@ public class GoodsController {
 
 	public static void main(String[] args) {
 		int i=10;
-
-		public void select() {
-			
-		}
+	}
+	
+	public void select() {
 		
 	}
 
---
+++ src/kosta/service/GoodsService.java
@@ -0,0 +1,11 @@
+package kosta.service;
+
+public class GoodsService {
+	public void insert() {
+		System.out.println("insert 메서드..");
+	}
+	public void select() {
+		System.out.println("select 메서드..");
+		System.out.println("시나리오 3번 test");
+	}
+}
