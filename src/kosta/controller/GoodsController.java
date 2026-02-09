77ca3341176c35ead2bcc9955b8cdec234a80089
jeje <pearl903425@gmail.com>
2026-02-09 17:05:32 +0900

    local에서 test메서드 충돌 해결

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -5,9 +5,9 @@ public class GoodsController {
 	public static void main(String[] args) {
 		int i=10;
 	}
-	
 	public void test() {
 		System.out.println("시나리오 5번 - 원격에서 test추가");
+		System.out.println("로컬에서 수정 중,,,");
 	}
 
 	public void selectAll() {
