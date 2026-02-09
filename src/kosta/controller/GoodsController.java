3580319b2634c7b442083081fd802c564aa89ed5
jeje <63036787+sensitiveCalc@users.noreply.github.com>
2026-02-09 16:57:46 +0900

    시나리오 5번 - 원격에서 test 추가

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -6,8 +6,8 @@ public class GoodsController {
 		int i=10;
 	}
 	
-	public void select() {
-			
+	public void test() {
+		System.out.println("시나리오 5번 - 원격에서 test추가");
 	}
 
 	public void selectAll() {
