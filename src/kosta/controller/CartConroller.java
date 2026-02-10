4b169954e0a8645bff1599b5e74b996dcd7dba9a
jeje <pearl903425@gmail.com>
2026-02-10 12:14:41 +0900

    OrderController 수정

--- src/kosta/controller/CartConroller.java
+++ src/kosta/controller/CartConroller.java
@@ -4,4 +4,8 @@ public class CartController {
 	public void insert() {
 		System.out.println("cart 브랜치 컨트롤러 추가");
 	}
+	
+	public void orderCart() {
+		System.out.println("로컬 cart 브랜치에서 추가함");
+	}
 }
