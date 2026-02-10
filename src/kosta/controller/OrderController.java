fe3c89c87cae1a407c351df323ede19d372bd3b1
jeje <63036787+sensitiveCalc@users.noreply.github.com>
2026-02-10 12:18:04 +0900

    Update OrderController.java

--- src/kosta/controller/OrderController.java
+++ src/kosta/controller/OrderController.java
@@ -4,4 +4,8 @@ public class OrderController {
 	public void orderInsert() {
 		System.out.println("order 브랜치에서 추가함");
 	}
+	
+	public void remoteCart() {
+		System.out.println("원격 cart 브랜치에서 추가함");
+	}
 }
